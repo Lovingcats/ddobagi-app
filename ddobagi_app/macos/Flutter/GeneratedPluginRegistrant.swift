d73d24adb4513eb0512102fac37fcaf9e7fbd75e
Lovingcats <whehdrms6810@naver.com>
2023-08-03 19:13:42 +0900

    [ADD] xcconfig

--- ddobagi_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ ddobagi_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,16 @@
 import FlutterMacOS
 import Foundation
 
+import dynamic_color
+import file_selector_macos
+import geolocator_apple
+import macos_ui
+import macos_window_utils
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
+  MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
 }
